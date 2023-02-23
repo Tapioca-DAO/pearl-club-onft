650c51dbde69f8afd5461de5581af5c2c31cb083
OxGAB <oxcryptogab@gmail.com>
2023-02-24 01:10:11 +0200

    ERC721 tests

--- test/PearlClubONFT.test.ts
+++ test/PearlClubONFT.test.ts
@@ -1,38 +1,70 @@
 import { expect, use } from 'chai'
 import { ethers } from 'hardhat'
 import { MerkleTree } from 'merkletreejs'
-import { padBuffer, register } from './test.utils'
+import { BN, padBuffer, register } from './test.utils'
 import { keccak256 } from 'ethers/lib/utils'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 use(require('chai-as-promised'))
 
 describe('Pearl Club ONFT test', function () {
-  it('allow only whitelisted accounts to mint once', async () => {
-    const {pearlClubONFT, tree, notWhitelisted, whitelisted, PearlClubONFT} = await loadFixture(register);
-    const merkleProof = tree.getHexProof(padBuffer(whitelisted[0].address))
-    const invalidMerkleProof = tree.getHexProof(padBuffer(notWhitelisted[0].address))
+  describe('Whitelist test', async () => {
+    it('allow only whitelisted accounts to mint once', async () => {
+      const { pearlClubONFT, tree, notWhitelisted, whitelisted, PearlClubONFT } = await loadFixture(register);
+      const merkleProof = tree.getHexProof(padBuffer(whitelisted[0].address))
+      const invalidMerkleProof = tree.getHexProof(padBuffer(notWhitelisted[0].address))
 
-    await expect(pearlClubONFT.mint(merkleProof)).to.not.be.rejected
-    await expect(pearlClubONFT.mint(merkleProof)).to.be.revertedWithCustomError(PearlClubONFT, "PearlClubONFT__AlreadyClaimed")
-    await expect(pearlClubONFT.connect(notWhitelisted[0]).mint(invalidMerkleProof)).to.be.revertedWithCustomError(PearlClubONFT, "PearlClubONFT__NotWhitelisted")
+      await expect(pearlClubONFT.mint(merkleProof)).to.not.be.rejected
+      await expect(pearlClubONFT.mint(merkleProof)).to.be.revertedWithCustomError(PearlClubONFT, "PearlClubONFT__AlreadyClaimed")
+      await expect(pearlClubONFT.connect(notWhitelisted[0]).mint(invalidMerkleProof)).to.be.revertedWithCustomError(PearlClubONFT, "PearlClubONFT__NotWhitelisted")
+    })
+    it('can change whitelist', async () => {
+      const { pearlClubONFT, tree, notWhitelisted, PearlClubONFT } = await loadFixture(register)
+      const invalidMerkleProof = tree.getHexProof(padBuffer(notWhitelisted[0].address))
+      await expect(pearlClubONFT.connect(notWhitelisted[0]).mint(invalidMerkleProof)).to.be.revertedWithCustomError(PearlClubONFT, "PearlClubONFT__NotWhitelisted")
+
+      tree.addLeaf(padBuffer(notWhitelisted[0].address))
+      const newMerkleRoot = tree.getHexRoot()
+      const validMerkleProof = tree.getHexProof(padBuffer(notWhitelisted[0].address))
+      await pearlClubONFT.setTreeRoot(newMerkleRoot);
+      await expect(pearlClubONFT.connect(notWhitelisted[0]).mint(validMerkleProof)).to.not.be.rejected;
+    })
+    it('only owner can change whitelist', async () => {
+      const { pearlClubONFT, tree, notWhitelisted, whitelisted } = await loadFixture(register)
+      const merkleRoot = tree.getHexRoot()
+      await expect(pearlClubONFT.setTreeRoot(merkleRoot)).to.not.be.rejected;
+      tree.addLeaf(padBuffer(notWhitelisted[0].address))
+      const newMerkleRoot = tree.getHexRoot()
+      await expect(pearlClubONFT.connect(whitelisted[1]).setTreeRoot(newMerkleRoot)).to.be.rejectedWith("Ownable: caller is not the owner");
+    })
   })
-  it('can change whitelist', async () => {
-    const {pearlClubONFT, tree, notWhitelisted, PearlClubONFT} = await loadFixture(register)
-    const invalidMerkleProof = tree.getHexProof(padBuffer(notWhitelisted[0].address))
-    await expect(pearlClubONFT.connect(notWhitelisted[0]).mint(invalidMerkleProof)).to.be.revertedWithCustomError(PearlClubONFT, "PearlClubONFT__NotWhitelisted")
-    
-    tree.addLeaf(padBuffer(notWhitelisted[0].address))
-    const newMerkleRoot = tree.getHexRoot()
-    const validMerkleProof = tree.getHexProof(padBuffer(notWhitelisted[0].address))
-    await pearlClubONFT.setTreeRoot(newMerkleRoot);
-    await expect(pearlClubONFT.connect(notWhitelisted[0]).mint(validMerkleProof)).to.not.be.rejected;
+  describe('Royality test', async () => {
+    it('returns correct royalty info', async () => {
+      const { pearlClubONFT, accounts } = await loadFixture(register)
+      await expect(pearlClubONFT.connect(accounts[2]).setRoyaltiesRecipient(accounts[1].address)).to.be.rejectedWith("Ownable: caller is not the owner");
+      await expect(pearlClubONFT.setRoyaltiesRecipient(accounts[1].address)).to.not.be.rejected;
+      const salePrice = 100000;
+      const result = await pearlClubONFT.royaltyInfo(0, salePrice);
+      expect(result[0]).to.equal(accounts[1].address)
+      expect(result[1]).to.equal(BN(salePrice).mul(5).div(100))
+    })
   })
-  it('only owner can change whitelist', async () => {
-    const {pearlClubONFT, tree, notWhitelisted, whitelisted} = await loadFixture(register)
-    const merkleRoot = tree.getHexRoot()
-    await expect(pearlClubONFT.setTreeRoot(merkleRoot)).to.not.be.rejected;
-    tree.addLeaf(padBuffer(notWhitelisted[0].address))
-    const newMerkleRoot = tree.getHexRoot()
-    await expect(pearlClubONFT.connect(whitelisted[1]).setTreeRoot(newMerkleRoot)).to.be.rejectedWith("Ownable: caller is not the owner");
+  describe('ERC721 functionality test', async () => {
+    it('is transferable', async () => {
+      const { pearlClubONFT, accounts, mintFor } = await register()
+      const recipient = accounts[1]
+      const { tokenID, to } = await mintFor()
+      await expect(pearlClubONFT.connect(to).transferFrom(to.address, recipient.address, tokenID)).to.not.be.rejected;
+      expect(await pearlClubONFT.balanceOf(recipient.address)).to.equal(BN(1))
+    })
+    it('has correct uri', async () => {
+      const { pearlClubONFT, mintFor } = await register()
+      const { tokenID } = await mintFor()
+      expect(await pearlClubONFT.tokenURI(tokenID)).to.equal(`ipfs://${process.env.METADATA_CID}/arb/${tokenID}`)
+    })
+  })
+  describe('ONFT functionality test', async () => {
+    it('can sent to other chains')
+    it('correctly tracks totalSupply')
+    it('changes background on different chains')
   })
 })
--- test/test.utils.ts
+++ test/test.utils.ts
@@ -1,22 +1,28 @@
 import { keccak256 } from "ethers/lib/utils"
 import { ethers } from "hardhat"
 import MerkleTree from "merkletreejs"
+import { BigNumberish, Wallet } from "ethers";
 
+function BN(n: BigNumberish) {
+  return ethers.BigNumber.from(n.toString());
+}
 const padBuffer = (addr: string) => {
     return Buffer.from(addr.substr(2).padStart(32*2, '0'), 'hex')
   }
 async function register() {
   const accounts = await ethers.getSigners()
+  const deployer = accounts[0]
     const whitelisted = accounts.slice(0, 5)
     const notWhitelisted = accounts.slice(5, 10)
     const leaves = whitelisted.map(account => padBuffer(account.address))
     const tree = new MerkleTree(leaves, keccak256, { sort: true })
     const merkleRoot = tree.getHexRoot()
     const PearlClubONFT = await ethers.getContractFactory('PearlClubONFT')
-    const pearlClubONFT = await PearlClubONFT.deploy(ethers.constants.AddressZero, "", 0, 10)
+    const pearlClubONFT = await PearlClubONFT.deploy(ethers.constants.AddressZero, `ipfs://${process.env.METADATA_CID}/arb/`, 0, 10, 350000)
     await pearlClubONFT.deployed()
     await pearlClubONFT.setTreeRoot(merkleRoot);
     const initalSetup = {
+      deployer,
       accounts,
       whitelisted,
       notWhitelisted,
@@ -24,6 +30,16 @@ async function register() {
       pearlClubONFT,
       PearlClubONFT
     }
-    return {...initalSetup}
+
+    async function mintFor(_to?: Wallet) {
+      const to = _to ?? whitelisted[0]
+      const tokenID = await pearlClubONFT.nextMintId() 
+      await pearlClubONFT.connect(to).mint(tree.getHexProof(padBuffer(to.address)))
+      return {to, tokenID}
+    }
+    const utilFuncs = {
+      mintFor
+    }
+    return {...initalSetup, ...utilFuncs}
 }
-export{padBuffer,register}
+export{padBuffer,register, BN}
