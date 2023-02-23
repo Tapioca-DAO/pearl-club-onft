6e5e0b646d426314af57d6719a7a3867fd054f89
OxGAB <oxcryptogab@gmail.com>
2023-02-23 03:33:18 +0200

    Add optional arg

--- tasks/getProof.ts
+++ tasks/getProof.ts
@@ -3,11 +3,11 @@ import { getTree } from "../scripts/utils";
 import { padBuffer } from "../scripts/utils";
 
 export const getProof__task = async (
-    taskArgs: any,
+    taskArgs: {foraddress?:string},
     hre: HardhatRuntimeEnvironment,
 ) => {
     const {ethers} = hre;
-    const address = (await ethers.getSigners())[0].address;
+    const address = taskArgs.foraddress ?? (await ethers.getSigners())[0].address;
     const tree = getTree();
     const proof = tree.getHexProof(padBuffer(address))
     console.log(`Proof for: ${address} \nis: ${proof}`);
