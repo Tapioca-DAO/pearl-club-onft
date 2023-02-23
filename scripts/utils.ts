b88897311f6fe52dc2d9ade672d2b3e3388f916e
OxGAB <oxcryptogab@gmail.com>
2023-02-24 01:09:31 +0200

    add getPassword func

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,16 +1,16 @@
 import { MerkleTree } from 'merkletreejs'
-import { writeFileSync } from "fs"
+import { writeFileSync, readFileSync } from 'fs';
 import { keccak256 } from 'ethers/lib/utils';
 const getTree = (log?: boolean) => {
     log = log ?? false;
-    let whitelistAddresses: string[] = require("../whitelist.json")
+    let whitelistAddresses: string[] = (readFileSync("whitelist.csv", {encoding: "utf-8"}) as string).split('\n')
     if(log) console.log(`Generating a tree from ${whitelistAddresses.length} whitelisted addresses...`)
     const leaves = whitelistAddresses.map(address => padBuffer(address))
     const tree = new MerkleTree(leaves, keccak256, { sort: true })
     if(log) console.log(`Generated tree: \n${tree.toString()}`)
     writeFileSync("tree.json", JSON.stringify(tree), { flag: "w" })
     if(log) console.log(`Tree saved to tree.json`);
-    return tree;
+    return {tree, whitelistAddresses};
 };
 const getOtherChainDeployment = (chainName: string) => {
     return require(`../deployments/${chainName}/PearlClubONFT.json`);
@@ -18,4 +18,12 @@ const getOtherChainDeployment = (chainName: string) => {
 const padBuffer = (addr: string) => {
     return Buffer.from(addr.substr(2).padStart(32*2, '0'), 'hex')
 }
-export { getTree, getOtherChainDeployment, padBuffer }
+function getPassword(_address: string) {
+    const passwords = JSON.parse(readFileSync("passwords.json", {encoding: "utf-8"})) as {
+        password: string;
+        proof: string;
+        address: string;
+    }[]
+    return passwords.find(({address}) => address == _address)?.password ?? "invalid address"
+}
+export { getTree, getOtherChainDeployment, padBuffer, getPassword }
