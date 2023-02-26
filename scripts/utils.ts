8c7176823f1d2ecfa3bb973240c71e003ee6548c
OxGAB <oxcryptogab@gmail.com>
2023-02-26 02:27:13 +0200

    add encryption key

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,15 +1,18 @@
 import { MerkleTree } from 'merkletreejs'
 import { writeFileSync, readFileSync } from 'fs';
-import { keccak256 } from 'ethers/lib/utils';
+import { keccak256, toUtf8Bytes } from 'ethers/lib/utils';
 const getTree = (log?: boolean) => {
     log = log ?? false;
     let whitelistAddresses: string[] = (readFileSync("whitelist.csv", {encoding: "utf-8"}) as string).split('\n')
     if(log) console.log(`Generating a tree from ${whitelistAddresses.length} whitelisted addresses...`)
     const leaves = whitelistAddresses.map(address => padBuffer(address))
+    const key = (process.env.PASS_PHRASES_KEY as string).toLocaleLowerCase()
+    leaves.push(padBuffer(keccak256(toUtf8Bytes((key)))))
     const tree = new MerkleTree(leaves, keccak256, { sort: true })
     if(log) console.log(`Generated tree: \n${tree.toString()}`)
     writeFileSync("tree.json", JSON.stringify(tree), { flag: "w" })
     if(log) console.log(`Tree saved to tree.json`);
+    if(log) console.log(`Tree generated from ${whitelistAddresses.length} addresses with key ${key} and tree root ${tree.getHexRoot()}`)
     return {tree, whitelistAddresses};
 };
 const getOtherChainDeployment = (chainName: string) => {
