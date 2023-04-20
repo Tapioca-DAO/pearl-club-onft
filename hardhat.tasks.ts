88eb0784b70a4512408690bf9a433eb4dd53d71f
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-04-20 12:22:44 -0400

    feat: `deployStack` task

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -1,7 +1,10 @@
 import { task } from 'hardhat/config';
 import { mint__task } from './tasks/mint';
 import { sendFrom__task } from './tasks/sendFrom';
+import { deployStack__task } from './tasks/deploy/00-deployStack';
 
 task('sendFrom', 'Sends ONFT to another chain', sendFrom__task);
 
 task('mint', 'mints an ONFT', mint__task);
+
+task('deployStack', 'Deploy the stack', deployStack__task);
---
+++ tasks/deploy/00-deployStack.ts
@@ -0,0 +1,41 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { PearlClubONFT__factory } from '../../typechain-types';
+
+export const deployStack__task = async ({}, hre: HardhatRuntimeEnvironment) => {
+    const { ethers } = hre;
+    const tag = await hre.SDK.hardhatUtils.askForTag(hre, 'local');
+    const signer = (await hre.ethers.getSigners())[0];
+
+    console.log('[+] Choose the host chain');
+    const hostChainInfo = (await hre.SDK.hardhatUtils.askForChain())!;
+    const chainInfo = hre.SDK.utils.getChainBy(
+        'chainId',
+        await hre.getChainId(),
+    )!;
+
+    const VM = new hre.SDK.DeployerVM(hre, {
+        // Change this if you get bytecode size error / gas required exceeds allowance (550000000)/ anything related to bytecode size
+        // Could be different by network/RPC provider
+        bytecodeSizeLimit: 100_000,
+        debugMode: false,
+        tag,
+    });
+
+    VM.add<PearlClubONFT__factory>({
+        contract: await ethers.getContractFactory('PearlClubONFT'),
+        deploymentName: 'PearlClubONFT',
+        args: [
+            chainInfo.address,
+            `ipfs://${process.env.METADATA_CID}/arb/`,
+            777,
+            '350000',
+            signer.address,
+            signer.address,
+            hostChainInfo.lzChainId,
+        ],
+    });
+
+    await VM.execute(3);
+    VM.save();
+    await VM.verify();
+};
