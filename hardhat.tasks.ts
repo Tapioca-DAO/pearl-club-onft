709e8cdf537cd2ef1811ab43f265a00993c6ec52
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-04-21 08:59:44 -0400

    chore: file path correction

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -2,8 +2,8 @@ import { task } from 'hardhat/config';
 import { mint__task } from './tasks/mint';
 import { sendFrom__task } from './tasks/sendFrom';
 import { deployStack__task } from './tasks/deploy/00-deployStack';
-import { jumpPhase__task } from './tasks/deploy/setups/01-jumpPhase';
-import { setClaim__task } from './tasks/deploy/setups/00-setClaim';
+import { setClaim__task } from './tasks/execs/00-setClaim';
+import { jumpPhase__task } from './tasks/execs/01-jumpPhase';
 
 task('sendFrom', 'Sends ONFT to another chain', sendFrom__task);
 
