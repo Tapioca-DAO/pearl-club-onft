332fed694a387126e8cdc804b52653d409b50a02
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-04-25 08:17:02 -0400

    test: `rescueMint()` test

--- test/PearlClubONFT.test.ts
+++ test/PearlClubONFT.test.ts
@@ -1,6 +1,7 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { register } from './test.utils';
+import hre from 'hardhat';
 
 describe('Pearl Club ONFT test', function () {
     describe('ONFT functionality test', async () => {
@@ -12,11 +13,31 @@ describe('Pearl Club ONFT test', function () {
                 pearlClubONFT1
                     .connect(accounts[2])
                     .mint(accounts[2].address, 1),
-            ).to.be.rejectedWith('PearlClubONFT__CallerNotMinter');
+            ).to.be.revertedWithCustomError(
+                pearlClubONFT1,
+                'PearlClubONFT__CallerNotMinter',
+            );
 
             await pearlClubONFT1.setClaimAvailable([deployer.address], true);
             await expect(pearlClubONFT1.mint(deployer.address, 1)).to.not.be
                 .rejected;
+        });
+
+        it('Minter can rescue mint after 30 days', async () => {
+            const { pearlClubONFT1, accounts, deployer } = await loadFixture(
+                register,
+            );
+
+            await expect(
+                pearlClubONFT1.rescueMint(accounts[2].address, 1),
+            ).to.be.revertedWithCustomError(
+                pearlClubONFT1,
+                'PearlClubONFT__RescueNotActive',
+            );
+
+            await time.increase(time.duration.days(30));
+            await expect(pearlClubONFT1.rescueMint(deployer.address, 1)).to.not
+                .be.rejected;
         });
     });
 });
