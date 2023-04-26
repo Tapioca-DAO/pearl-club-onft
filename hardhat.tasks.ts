b35e609da52f23c576fa862edeca3809823616b0
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-04-25 22:12:33 -0400

    rescue mission

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -5,6 +5,7 @@ import { deployStackTestnet__task } from './tasks/deploy/00-deployStackTestnet';
 import { setClaim__task } from './tasks/execs/00-setClaim';
 import { deployStack__task } from './tasks/deploy/00-deployStack';
 import { setBulkClaim__task } from './tasks/execs/01-setBulkClaim';
+import { checkLost__task } from './tasks/execs/tsd';
 
 task('sendFrom', 'Sends ONFT to another chain', sendFrom__task);
 
@@ -18,3 +19,4 @@ task(
 task('deployStack', 'Deploy the stack on mainnet', deployStack__task);
 task('setClaim', 'Set a claim list', setClaim__task);
 task('setBulkClaim', 'Set the claim list in bulk', setBulkClaim__task);
+task('checkLost', '', checkLost__task);
